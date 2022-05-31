3006d8ac5e073783be983f0f766f2aaab24c565d
Felipe Cruz <felipephplow@gmail.com>
2022-05-31 16:28:33 -0300

    a

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>123 Lançe</title>
+    <title>123LANCE</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
