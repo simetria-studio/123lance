84667473c6c2de4a1a0d05c55f8d0297f047871d
Felipe Cruz <felipephplow@gmail.com>
2022-05-31 16:01:13 -0300

    first commit

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,252 @@
+@extends('layouts.main')
+@section('content')
+    <div class="hero">
+        <div class="container">
+            <div class="hero-grid">
+                <div>
+
+                </div>
+                <div class="text-hero">
+                    <div>
+                        <h1>Ofertas<br> incríves<br> em todo<br> nosso site</h1>
+                    </div>
+                    <div>
+                        <button class="btn btn-one">cadastre-se</button>
+                    </div>
+                </div>
+            </div>
+            <div class="strip">
+                <div class="container">
+                    <div class="strip-grid">
+                        <div class="strip-block">
+                            <div>
+                                <img src="{{ asset('front/img/martelo-02.svg') }}" alt="">
+                            </div>
+                            <div>
+                                <h5>Pague com PIX<br> tenha 10% a mais<br> em lances</h5>
+                            </div>
+                        </div>
+                        <div class="strip-block">
+                            <div>
+                                <img src="{{ asset('front/img/martelo-02.svg') }}" alt="">
+                            </div>
+                            <div>
+                                <h5>Garantimos a entrega<br> de seus arremates em<br> todo Brasil</h5>
+                            </div>
+                        </div>
+                        <div class="strip-block">
+                            <div>
+                                <img src="{{ asset('front/img/martelo-02.svg') }}" alt="">
+                            </div>
+                            <div>
+                                <h5>Após o arremate você<br> escolhe entre receber<br> o produto ou valor</h5>
+                            </div>
+                        </div>
+                        <div class="strip-block">
+                            <div>
+                                <img src="{{ asset('front/img/martelo-02.svg') }}" alt="">
+                            </div>
+                            <div>
+                                <h5>Faça seu cadastro é gratuito<br> fácil e rápido e você pode<br> fazer seus lances</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="filters">
+        <div class="container">
+            <div class="filter-grid">
+                <div>
+                    <a href="">Leilões Online</a>
+                    <a href="">Próximos leilões</a>
+                    <a href="">Leilões Arrematados</a>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <div class="all">
+                        <a href=""><span><i class="fa-solid fa-arrow-right-long"></i></span> ver todos</a>
+                    </div>
+                    <div class="navigation">
+                        <a href=""><i class="fa-solid fa-angle-left"></i></a>
+                        <a href=""><i class="fa-solid fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="products">
+        <div class="container">
+            <div class="product-grid">
+                <div class="product-body">
+                    <div class="product-header">
+                        <div>
+                            <h5><span><i class="fa-solid fa-calendar"></i></span> 12/05/2022</h5>
+                        </div>
+                        <div>
+                            <h5><span><i class="fa-solid fa-clock"></i></span> 13:05:00</h5>
+                        </div>
+                    </div>
+                    <div class="product-img">
+                        <img src="{{ asset('front/img/playstation.png') }}" alt="">
+                    </div>
+                    <div class="product-desc">
+                        <p>Console PlayStation 5 Controle Dual Sense PS5 Branco e Preto</p>
+                        <p>Luis Carlos 2022</p>
+                        <h3>R$ 0,17</h3>
+                    </div>
+                    <div class="product-footer">
+                        <div>
+                            <button class="btn btn-two">Faça seu lance</button>
+                        </div>
+                        <div>
+                            <h4><span>1</span><span>5</span></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="product-body">
+                    <div class="product-header">
+                        <div>
+                            <h5><span><i class="fa-solid fa-calendar"></i></span> 12/05/2022</h5>
+                        </div>
+                        <div>
+                            <h5><span><i class="fa-solid fa-clock"></i></span> 13:05:00</h5>
+                        </div>
+                    </div>
+                    <div class="product-img">
+                        <img src="{{ asset('front/img/playstation.png') }}" alt="">
+                    </div>
+                    <div class="product-desc">
+                        <p>Console PlayStation 5 Controle Dual Sense PS5 Branco e Preto</p>
+                        <p>Luis Carlos 2022</p>
+                        <h3>R$ 0,17</h3>
+                    </div>
+                    <div class="product-footer">
+                        <div>
+                            <button class="btn btn-three">Faça seu lance</button>
+                        </div>
+                        <div>
+                            <h4><span>1</span><span>5</span></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="product-body">
+                    <div class="product-header">
+                        <div>
+                            <h5><span><i class="fa-solid fa-calendar"></i></span> 12/05/2022</h5>
+                        </div>
+                        <div>
+                            <h5><span><i class="fa-solid fa-clock"></i></span> 13:05:00</h5>
+                        </div>
+                    </div>
+                    <div class="product-img">
+                        <img src="{{ asset('front/img/playstation.png') }}" alt="">
+                    </div>
+                    <div class="product-desc">
+                        <p>Console PlayStation 5 Controle Dual Sense PS5 Branco e Preto</p>
+                        <p>Luis Carlos 2022</p>
+                        <h3>R$ 0,17</h3>
+                    </div>
+                    <div class="product-footer">
+                        <div>
+                            <button class="btn btn-two">Faça seu lance</button>
+                        </div>
+                        <div>
+                            <h4><span>1</span><span>5</span></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="testimonials">
+        <div class="container">
+            <div class="grid-testimonials">
+                <div class="left">
+                    <div class="text-testimonials">
+                        <h2>Como<br> <span>funciona?</span></h2>
+                        <p>Faça seu cadastro e<br> ganhe 5 lances bônus<br> para começar</p>
+                        <button class="btn btn-two">Cadastre-se</button>
+                    </div>
+                    <div>
+                        <img src="{{ asset('front/img/mulher.png') }}" alt="">
+                    </div>
+                </div>
+                <div>
+                    <div class="video">
+                        <div class="play">
+                            <img src="{{ asset('front/img/Vector.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fillet">
+        <div class="container">
+            <div class="fillet-grid">
+                <div class="block-fillet">
+                    <div>
+                        <img src="{{ asset('front/img/check.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <p>Faça seu cadastro</p>
+                    </div>
+                </div>
+                <div class="block-fillet">
+                    <div>
+                        <img src="{{ asset('front/img/check.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <p>Compre pacote de lances</p>
+                    </div>
+                </div>
+                <div class="block-fillet">
+                    <div>
+                        <img src="{{ asset('front/img/check.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <p>Participe dos leilões</p>
+                    </div>
+                </div>
+                <div class="block-fillet">
+                    <div>
+                        <img src="{{ asset('front/img/check.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <p>Divirta-se</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="providers">
+        <div class="container">
+            <div class="providers-grid">
+                <div>
+                    <h4>Nossos<br> <span>fornecedores</span> </h4>
+                </div>
+                <div class="grid-logo">
+                    <div class="logos">
+                        <img src="{{ asset('front/img/magazine-luiza.svg') }}" alt="">
+                    </div>
+                    <div class="logos">
+                        <img src="{{ asset('front/img/casas-bahia.svg') }}" alt="">
+                    </div>
+                    <div class="logos">
+                        <img src="{{ asset('front/img/ponto-frio.svg') }}" alt="">
+                    </div>
+                    <div class="logos">
+                        <img src="{{ asset('front/img/amazon.svg') }}" alt="">
+                    </div>
+                    <div class="logos">
+                        <img src="{{ asset('front/img/aliexpress.svg') }}" alt="">
+                    </div>
+                    <div class="logos">
+                        <img src="{{ asset('front/img/shoppe.svg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>123 Lançe</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/0ab2bcde1c.js" crossorigin="anonymous"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('front/css/main.min.css') }}">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-grid">
+                <div>
+                    <img src="{{ asset('front/img/logo.svg') }}" alt="">
+                </div>
+                <div class="d-flex">
+                    <div>
+                        <img src="{{ asset('front/img/martelo.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <h4>Leilões <span>Online</span></h4>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <div>
+                        <img src="{{ asset('front/img/martelo.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <h4>Leilões <span>Arrematados</span> </h4>
+                    </div>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+                <div class="d-flex">
+                    <div class="mx-1">
+                        <button class="btn btn-one">Faça Login</button>
+                    </div>
+                    <div class="mx-1">
+                        <button class="btn btn-two">Compre Lance</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <div class="container">
+            <div class="footer-grid">
+                <div class="block-footer">
+                    <h2>Institucional</h2>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Quem Somos</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Depoimentos</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Blog</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Contato</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Termos de Uso</a>
+                </div>
+                <div class="block-footer">
+                    <h2>Como Funciona</h2>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Passo a Passo</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Perguntas Frequentes</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Dicas e Estratégias</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Minha Conta</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Segurança e Entrega</a>
+                </div>
+                <div class="block-footer">
+                    <h2>Termos de uso</h2>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Política de privacidade</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Políticas de cookies</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Termos e condições</a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Politica de entrega </a>
+                    <a href=""><span><i class="fa-solid fa-arrow-right"></i></span>Política de reembolso</a>
+                </div>
+                <div class="block-footer">
+                    <h2>Suporte</h2>
+                    <a href="">E-mail</a>
+                    <a href="">Telefone</a>
+                    <a href="">Whatsapp</a>
+                    <a href="">Ticket</a>
+                </div>
+                <div class="block-footer">
+                    <h2>Forma de pagamento</h2>
+                    <div>
+                        <img src="{{ asset('front/img/pix.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <img src="{{ asset('front/img/pagamentos.svg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</body>
+
+</html>
