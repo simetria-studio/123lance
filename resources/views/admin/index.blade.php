09377d38e6666f9227deb37ca078cf3397fb29b7
Felipe Cruz <felipephplow@gmail.com>
2022-06-02 16:03:03 -0300

    a

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,6 +11,190 @@
                 </p>
             </div>
         </div>
+        <div class="d-flex mt-5 menu">
+            <div class="mt-1">
+                <img src="{{ asset('painel/img/ri_logout-circle-r-fill.svg') }}" alt="">
+            </div>
+            <div class="mx-2">
+                <p>Sair do<br>
+                    <span>Sistema</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mt-5">
+                <div class="block-1">
+                    <div class="node-1">
+                        <div>
+                            <p>Top 100<br>
+                                <span>Clientes x Lances</span>
+                            </p>
+                        </div>
+                        <div>
+                            <a href="">Ver todos</a>
+                        </div>
+                    </div>
+                    <div class="node">
+                        <div>
+                            <p>Usuário</p>
+                        </div>
+                        <div>
+                            <p>Lances</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mt-5">
+                <div class="block-1">
+                    <div class="node-1">
+                        <div>
+                            <p>Top 100<br>
+                                <span>Clientes x Arremates</span>
+                            </p>
+                        </div>
+                        <div>
+                            <a href="">Ver todos</a>
+                        </div>
+                    </div>
+                    <div class="node">
+                        <div>
+                            <p>Usuário</p>
+                        </div>
+                        <div>
+                            <p>Lances</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>lcvpaim</p>
+                        </div>
+                        <div>
+                            <p>1200</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mt-5">
+                <div class="block-1">
+                    <div class="node-1">
+                        <div>
+                            <p>Lances<br>
+                                <span>Vendidos x utilizados</span>
+                            </p>
+                        </div>
+                        <div>
+                            <a href="">Ver todos</a>
+                        </div>
+                    </div>
+                    <div class="node">
+                        <div>
+                            <p >Vendidos</p>
+                        </div>
+                        <div>
+                            <p>utilizados</p>
+                        </div>
+                    </div>
+                    <div class="node-2">
+                        <div>
+                            <p>200.000</p>
+                        </div>
+                        <div>
+                            <p>120.000</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
-    
