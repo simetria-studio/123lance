05a59f3dc613c21cadc5b9370bd4384e53fd6944
Felipe Cruz <felipephplow@gmail.com>
2022-06-06 14:23:02 -0300

    a

--- resources/views/front/partials/hero-mobile.blade.php
+++ resources/views/front/partials/hero-mobile.blade.php
@@ -1,10 +1,12 @@
 <div class="m-hero">
     <div class="container">
         <div class="call">
-            <h4>Leilão de produtos
-                novos com até
-                90% de desconto</h4>
+            <h4>Leilão de produtos<br><span>
+                    novos com até
+                    90% de desconto</span></h4>
+            <div class="text-center">
                 <button class="btn btn-two">Compre Lance</button>
+            </div>
         </div>
     </div>
 </div>
