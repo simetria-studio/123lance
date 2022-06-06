1e72e47983dc4f1bc4bbd4e80298211524661c86
Felipe Cruz <felipephplow@gmail.com>
2022-06-06 15:35:14 -0300

    daw

--- resources/views/layouts/partials/header-mobile.blade.php
+++ resources/views/layouts/partials/header-mobile.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row block-m">
             <div class="col-6">
-                <img src="{{ asset('front/img/logo.svg') }}" alt="">
+                <a href="{{ route('index') }}"> <img src="{{ asset('front/img/logo.svg') }}" alt=""></a>
             </div>
             <div class="col-6 txt-align" id="btn-menu">
                 <h4>Menu <span><img src="{{ asset('front/img/menu.svg') }}" alt=""></span></h4>
@@ -34,5 +34,32 @@
             <a href="">Como funciona?</a>
             <div class="underline"></div>
         </div>
+    </div>
+    <div class="menu-footer d-flex flex-column">
+        <div class="d-flex align-items-center">
+            <div>
+                <img src="{{ asset('front/img/martelo-3.svg') }}" alt="">
+            </div>
+            <div class="ms-1">
+                <p>Pague com PIX tenha 10% a mais em lances</p>
+            </div>
+        </div>
+        <div class="d-flex align-items-center">
+            <div>
+                <img src="{{ asset('front/img/martelo-3.svg') }}" alt="">
+            </div>
+            <div class="ms-1">
+                <p>Garantimos a entrega de seus arremates em todo Brasil</p>
+            </div>
+        </div>
+        <div class="d-flex align-items-center">
+            <div>
+                <img src="{{ asset('front/img/martelo-3.svg') }}" alt="">
+            </div>
+            <div class="ms-1">
+                <p>Após o arremate você escolhe entre receber o produto ou valor</p>
+            </div>
+        </div>
+
     </div>
 </div>
