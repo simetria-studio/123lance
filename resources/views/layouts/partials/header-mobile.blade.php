2db53cbc428d4a0a0c9c0544450ecd21567d0e9b
Felipe Cruz <felipephplow@gmail.com>
2022-06-06 14:16:14 -0300

    a

---
+++ resources/views/layouts/partials/header-mobile.blade.php
@@ -0,0 +1,12 @@
+<div class="m-header">
+    <div class="container">
+        <div class="row block-m">
+            <div class="col-6">
+                <img src="{{ asset('front/img/logo.svg') }}" alt="">
+            </div>
+            <div class="col-6 txt-align">
+                <h4>Menu <span><img src="{{ asset('front/img/menu.svg') }}" alt=""></span></h4>
+            </div>
+        </div>
+    </div>
+</div>
