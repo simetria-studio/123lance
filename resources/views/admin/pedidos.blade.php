dd89321896e7615182cf3945553f8a2cb71e43d3
Felipe Cruz <felipephplow@gmail.com>
2022-06-07 17:18:01 -0300

    fasf

--- resources/views/admin/pedidos.blade.php
+++ resources/views/admin/pedidos.blade.php
@@ -3,12 +3,22 @@
     <div class="container mt-5">
         <div class="filters-group">
             <div class="row">
-                <div class="col-12 col-md-8">
-                    <button class="btn btn-one mx-1">Todos</button>
-                    <button class="btn btn-two mx-1">Completos</button>
-                    <button class="btn btn-three mx-1">Pendentes</button>
-                    <button class="btn btn-four mx-1">Cancelados</button>
-                    <button class="btn btn-five mx-1">Reembolsados</button>
+                <div class="col-12 col-md-8 m-filters">
+                    <div>
+                        <button class="btn btn-one mx-1">Todos</button>
+                    </div>
+                    <div>
+                        <button class="btn btn-two mx-1">Completos</button>
+                    </div>
+                    <div>
+                        <button class="btn btn-three mx-1">Pendentes</button>
+                    </div>
+                    <div>
+                        <button class="btn btn-four mx-1">Cancelados</button>
+                    </div>
+                    <div>
+                        <button class="btn btn-five mx-1">Reembolsados</button>
+                    </div>
 
                 </div>
             </div>
@@ -60,48 +70,22 @@
         <div class="pedidos">
             <div class="pedidos-grid">
                 <div>
-                   <p>#871345</p>
+                    <p>#871345</p>
                 </div>
                 <div>
-                   <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
+                    <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
                 </div>
                 <div>
-                   <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
+                    <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
                 </div>
                 <div>
-                   <p>200 Lances</p>
+                    <p>200 Lances</p>
                 </div>
                 <div>
-                   <p>R$ 200,00</p>
+                    <p>R$ 200,00</p>
                 </div>
                 <div>
-                   <p>200,00</p>
-                </div>
-                <div>
-                    <button class="btn btn-five mx-1">Reembolsados</button>
-                </div>
-                <div>
-                    <a href="">...</a>
-                </div>
-            </div>
-            <div class="pedidos-grid">
-                <div>
-                   <p>#871345</p>
-                </div>
-                <div>
-                   <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
-                </div>
-                <div>
-                   <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
-                </div>
-                <div>
-                   <p>200 Lances</p>
-                </div>
-                <div>
-                   <p>R$ 200,00</p>
-                </div>
-                <div>
-                   <p>200,00</p>
+                    <p>200,00</p>
                 </div>
                 <div>
                     <button class="btn btn-five mx-1">Reembolsados</button>
@@ -112,22 +96,48 @@
             </div>
             <div class="pedidos-grid">
                 <div>
-                   <p>#871345</p>
+                    <p>#871345</p>
                 </div>
                 <div>
-                   <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
+                    <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
                 </div>
                 <div>
-                   <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
+                    <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
                 </div>
                 <div>
-                   <p>200 Lances</p>
+                    <p>200 Lances</p>
                 </div>
                 <div>
-                   <p>R$ 200,00</p>
+                    <p>R$ 200,00</p>
                 </div>
                 <div>
-                   <p>200,00</p>
+                    <p>200,00</p>
+                </div>
+                <div>
+                    <button class="btn btn-five mx-1">Reembolsados</button>
+                </div>
+                <div>
+                    <a href="">...</a>
+                </div>
+            </div>
+            <div class="pedidos-grid">
+                <div>
+                    <p>#871345</p>
+                </div>
+                <div>
+                    <p><span><i class="fa-solid fa-user"></i></span> lcvpaim</p>
+                </div>
+                <div>
+                    <p><span><i class="fa-solid fa-calendar-days"></i></span> 15/05/2022</p>
+                </div>
+                <div>
+                    <p>200 Lances</p>
+                </div>
+                <div>
+                    <p>R$ 200,00</p>
+                </div>
+                <div>
+                    <p>200,00</p>
                 </div>
                 <div>
                     <button class="btn btn-five mx-1">Reembolsados</button>
